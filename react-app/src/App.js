e309f028cfdfdb925a846e13ff7edb28771a7814
Paul <dustin.paul.oh@gmail.com>
2022-04-12 09:43:16 -0700

    more import fixes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
+import Modal from './components/Modal'
+
 import { authenticate } from './store/session';
 
 function App() {
