b57e5b88bf498628a5025af787a8d08596e399ee
Paul <dustin.paul.oh@gmail.com>
2022-04-18 23:43:40 -0700

    fixed dynamic error handling

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { signUp } from "../../store/session";
 import { setCurrentModal, hideModal } from "../../store/modal";
@@ -7,51 +7,84 @@ import LoginForm from "./LoginForm";
 import styles from './Auth.module.css'
 
 const SignUpForm = () => {
-	const [errors, setErrors] = useState([]);
+  const firstRender = useRef(true)
+
 	const [firstName, setFirstName] = useState("");
+  const [firstNameError, setFirstNameError] = useState("")
+
 	const [lastName, setLastName] = useState("");
+  const [lastNameError, setLastNameError] = useState("")
+
 	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("")
+
+	const [password, setPassword] = useState("")
 	const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("")
+
   const [disabled, setDisabled] = useState(true)
 	const dispatch = useDispatch();
 
 	const loginDemo = async (e) => {
 		e.preventDefault();
-		const data = await dispatch(login("demo@aa.io", "password"));
-		if (data) return setErrors(data);
+		await dispatch(login("demo@aa.io", "password"));
 		dispatch(hideModal());
 	};
 
   useEffect(() => {
-    if (!firstName) {
-      errors.push('First name is required')
+    if (firstRender.current) {
+      firstRender.current = false
+      return
     }
-    if (!lastName) {
-      errors.push('Last name is required')
-    }
-    if (!email) {
-      errors.push('Email is required')
-    }
-    if (password !== confirmPassword) {
-      errors.push('Password must match')
-    }
-    else {
-      setErrors([])
+
+    if (firstName.length < 1) {
+      setFirstNameError("You missed a spot! Don't forget to tell us who you are!")
+      setDisabled(true)
+      return
+    } else {
       setDisabled(false)
+      setFirstNameError('')
+    }
+
+    if (lastName.length < 1) {
+      setLastNameError("You missed a spot! Don't forget to tell us your last name!")
+      setDisabled(true)
+      return
+    } else {
+      setDisabled(false)
+      setLastNameError('')
+    }
+
+    if (email.length < 1) {
+      setEmailError("You missed a spot! Don't forget to add your email!")
+      setDisabled(true)
+      return
+    } else {
+      setDisabled(false)
+      setEmailError('')
+    }
+
+    if (password !== confirmPassword) {
+      setPasswordError("You missed a spot! Make sure your passwords match!")
+      setDisabled(true)
+    } else {
+      setPasswordError('')
+      setDisabled(false)
+      console.log('validations finished')
+      return
     }
   }, [firstName, lastName, email, password, confirmPassword])
 
 
 	const handleSubmit = () => {
     // if (password === confirmPassword ? )
-    if (password === confirmPassword) {
-      const data = dispatch(signUp(firstName, lastName, email, password, confirmPassword));
+    // if (password === confirmPassword) {
+      dispatch(signUp(firstName, lastName, email, password, confirmPassword));
       dispatch(hideModal());
-      if (data) {
-        return setErrors(data);
-      }
-    }
+      // if (data) {
+        // return setErrors(data);
+      // }
+    // }
 	};
 
 	const showLoginForm = () => {
@@ -61,13 +94,6 @@ const SignUpForm = () => {
 	return (
 		<div className={styles.parent}>
 			<h4>Welcome to Pynterest!</h4>
-			{errors.length > 0 && (
-				<div className={styles.error_container}>
-					{errors.map((error, i) => (
-						<div key={i}>{error.replace("_", " ")}</div>
-					))}
-				</div>
-			)}
 			<form className={styles.signup_form}>
 				<div className={styles.fields}>
 					<div>
@@ -77,7 +103,8 @@ const SignUpForm = () => {
 							name="firstName"
 							onChange={(e) => setFirstName(e.target.value)}
 							value={firstName}
-						></input>
+						></input> <br />
+            <div>{firstNameError}</div>
 					</div>
 					<div>
 						<label>Last Name</label>
@@ -86,7 +113,8 @@ const SignUpForm = () => {
 							name="lastName"
 							onChange={(e) => setLastName(e.target.value)}
 							value={lastName}
-						></input>
+						></input> <br />
+            <div>{lastNameError}</div>
 					</div>
 					<div>
 						<label>Email</label>
@@ -95,7 +123,8 @@ const SignUpForm = () => {
 							name="email"
 							onChange={(e) => setEmail(e.target.value)}
 							value={email}
-						></input>
+						></input> <br />
+            <div>{emailError}</div>
 					</div>
 					<div>
 						<label>Password</label>
@@ -115,13 +144,13 @@ const SignUpForm = () => {
 							onChange={(e) => setConfirmPassword(e.target.value)}
 							autoComplete="none"
 							value={confirmPassword}
-						></input>
+						></input> <br />
+            <div>{passwordError}</div>
 					</div>
 				</div>
 				<div
 					className={styles.div_button}
-					onClick={handleSubmit}
-          disabled={disabled}
+					onClick={disabled && handleSubmit}
 				>
 					Sign Up
 				</div>
