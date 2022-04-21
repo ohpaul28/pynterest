9561f34125149601c2e3912420b33414add5ec62
Paul <dustin.paul.oh@gmail.com>
2022-04-20 21:09:29 -0700

    started on single board

--- react-app/src/components/SingleBoard/index.js
+++ react-app/src/components/SingleBoard/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
-// import {useDispatch} from 'react-redux';
+import React, { useContext }from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import styles from './SingleBoard.module.css'
+import SelectedContext from '../context/selectedContext';
+import { Pyns } from '../Homepage/tabs/Pyns';
 
 
 
-export const SingleBoard = ({ board }) => {
+export const SingleBoard = ({ boardId }) => {
+  const {setSelected} = useContext(SelectedContext);
+  const boards = useSelector(state => state.boards)
+  const pyns = useSelector(state => state.pyns)
+  const displayedBoard = boards[boardId]
+  const selectedPyns = []
+
+  displayedBoard['pyns'].forEach(pynId => {
+    selectedPyns.push(pyns[pynId])
+  })
+
 
 
 
   return (
-    <div>
-      <h1>hello from single board</h1>
-      {}
+    <div className={styles.parent}>
+      <div className={styles.div1}></div>
+      <div className={styles.div2}></div>
+      <div className={styles.div3}></div>
+      <div className={styles.div4}></div>
+      <div className={styles.div5}></div>
+      <div className={styles.div6}></div>
+      <div className={styles.div7}></div>
     </div>
   );
 
