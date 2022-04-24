be5ac148138fecf97ea645b219f9e4f7aa47687f
Paul <dustin.paul.oh@gmail.com>
2022-04-24 14:02:58 -0700

    heroku push

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {Link} from 'react-router-dom';
 
 import {LoginForm} from '../Forms/LoginForm'
 import {SignUpForm} from '../Forms/SignUpForm'
